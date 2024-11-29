e1b020dcbcf4f6eb1ad4c872d16343823ecc61fb
raphael <gamehotdl@gmail.com>
2024-11-29 12:05:55 +0100

    fix: Fix regex pattern for IconField introspection rules

--- fontawesome_5/models.py
+++ fontawesome_5/models.py
@@ -2,7 +2,7 @@
 try:
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([], [
-        "^fontawesome_5\.fields\.IconField" # pylint: disable=anomalous-backslash-in-string
+        r"^fontawesome_5\.fields\.IconField"
     ])
 except ImportError:
     pass
